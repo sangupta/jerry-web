fbfb29bf8e1748da3ca7f2cff51e2dfd984608ba
Sandeep Gupta <sandy.pec@gmail.com>
2014-02-26 18:17:03 +0530

    Added a javascript reordering filter that moves all javascript files and tags to the end of the HTML

--- src/main/java/com/sangupta/jerry/web/ByteArrayServletOutputStream.java
+++ src/main/java/com/sangupta/jerry/web/ByteArrayServletOutputStream.java
@@ -45,11 +45,17 @@ public class ByteArrayServletOutputStream extends ServletOutputStream {
 	private final PrintWriter writer = new PrintWriter(this.outputStream);
 	
 	/**
+	 * Check if the writer has been obtained from this stream
+	 */
+	private volatile boolean writerObtained = false;
+	
+	/**
 	 * Return the writer object associated with this.
 	 * 
 	 * @return
 	 */
 	public PrintWriter getWriter() {
+		this.writerObtained = true;
 		return this.writer;
 	}
 	
@@ -59,6 +65,10 @@ public class ByteArrayServletOutputStream extends ServletOutputStream {
 	 * @return
 	 */
 	public int getLength() {
+		if (this.writerObtained) {
+			this.writer.flush();
+		}
+		 
 		return this.outputStream.size();
 	}
 	
@@ -71,192 +81,273 @@ public class ByteArrayServletOutputStream extends ServletOutputStream {
 		return this.outputStream;
 	}
 	
+	/**
+	 * Get the raw bytes written to this stream.
+	 * 
+	 * @return
+	 */
+	public byte[] getBytes() {
+		if (this.writerObtained) {
+			this.writer.flush();
+		}
+
+		return this.outputStream.toByteArray();
+	}
+	
 	//-----------------------------------------
 	// Overridden methods follow
 	//-----------------------------------------
 
 	/**
-	 * @see java.io.OutputStream#write(int)
-	 */
-	@Override
-	public void write(int value) throws IOException {
-		this.outputStream.write(value);
-		
-	}
+     * @see java.io.OutputStream#write(int)
+     */
+    @Override
+    public void write(int value) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+        this.outputStream.write(value);
+    }
 
-	/**
-	 * @see java.io.OutputStream#write(byte[])
-	 */
-	@Override
-	public void write(byte[] b) throws IOException {
-		this.outputStream.write(b);
-	}
-	
-	/**
-	 * @see java.io.OutputStream#write(byte[], int, int)
-	 */
-	@Override
-	public void write(byte[] b, int off, int len) throws IOException {
-		this.outputStream.write(b, off, len);
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(boolean)
-	 */
-	@Override
-	public void print(boolean b) throws IOException {
-		this.writer.print(b);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(char)
-	 */
-	@Override
-	public void print(char c) throws IOException {
-		this.writer.print(c);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(double)
-	 */
-	@Override
-	public void print(double d) throws IOException {
-		this.writer.print(d);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(float)
-	 */
-	@Override
-	public void print(float f) throws IOException {
-		this.writer.print(f);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(int)
-	 */
-	@Override
-	public void print(int i) throws IOException {
-		this.writer.print(i);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(long)
-	 */
-	@Override
-	public void print(long l) throws IOException {
-		this.writer.print(l);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#print(java.lang.String)
-	 */
-	@Override
-	public void print(String s) throws IOException {
-		this.writer.print(s);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println()
-	 */
-	@Override
-	public void println() throws IOException {
-		this.writer.println();
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(boolean)
-	 */
-	@Override
-	public void println(boolean b) throws IOException {
-		this.writer.println(b);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(char)
-	 */
-	@Override
-	public void println(char c) throws IOException {
-		this.writer.println(c);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(double)
-	 */
-	@Override
-	public void println(double d) throws IOException {
-		this.writer.println(d);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(float)
-	 */
-	@Override
-	public void println(float f) throws IOException {
-		this.writer.println(f);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(int)
-	 */
-	@Override
-	public void println(int i) throws IOException {
-		this.writer.println(i);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(long)
-	 */
-	@Override
-	public void println(long l) throws IOException {
-		this.writer.println(l);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see javax.servlet.ServletOutputStream#println(java.lang.String)
-	 */
-	@Override
-	public void println(String s) throws IOException {
-		this.writer.println(s);
-		this.writer.flush();
-	}
-	
-	/**
-	 * @see java.io.OutputStream#flush()
-	 */
-	@Override
-	public void flush() throws IOException {
-		this.writer.flush();
-		this.outputStream.flush();
-	}
-	
-	/**
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		return this.outputStream.hashCode();
-	}
-	
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		return this.outputStream.equals(obj);
-	}
+    /**
+     * @see java.io.OutputStream#write(byte[])
+     */
+    @Override
+    public void write(byte[] b) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+        this.outputStream.write(b);
+    }
+
+    /**
+     * @see java.io.OutputStream#write(byte[], int, int)
+     */
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+        this.outputStream.write(b, off, len);
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(boolean)
+     */
+    @Override
+    public void print(boolean b) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(b);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(char)
+     */
+    @Override
+    public void print(char c) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(c);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(double)
+     */
+    @Override
+    public void print(double d) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(d);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(float)
+     */
+    @Override
+    public void print(float f) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(f);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(int)
+     */
+    @Override
+    public void print(int i) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(i);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(long)
+     */
+    @Override
+    public void print(long l) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(l);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#print(java.lang.String)
+     */
+    @Override
+    public void print(String s) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.print(s);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println()
+     */
+    @Override
+    public void println() throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println();
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(boolean)
+     */
+    @Override
+    public void println(boolean b) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(b);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(char)
+     */
+    @Override
+    public void println(char c) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(c);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(double)
+     */
+    @Override
+    public void println(double d) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(d);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(float)
+     */
+    @Override
+    public void println(float f) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(f);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(int)
+     */
+    @Override
+    public void println(int i) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(i);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(long)
+     */
+    @Override
+    public void println(long l) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(l);
+        this.writer.flush();
+    }
+
+    /**
+     * @see javax.servlet.ServletOutputStream#println(java.lang.String)
+     */
+    @Override
+    public void println(String s) throws IOException {
+        if(this.writerObtained) {
+            this.writer.flush();
+        }
+
+        this.writer.println(s);
+        this.writer.flush();
+    }
+
+    /**
+     * @see java.io.OutputStream#flush()
+     */
+    @Override
+    public void flush() throws IOException {
+        this.writer.flush();
+        this.outputStream.flush();
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return this.outputStream.hashCode();
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return this.outputStream.equals(obj);
+    }
 }
---
+++ src/main/java/com/sangupta/jerry/web/filters/JavascriptReorderingFilter.java
@@ -0,0 +1,260 @@
+/**
+ *
+ * jerry - Common Java Functionality
+ * Copyright (c) 2012, Sandeep Gupta
+ * 
+ * http://www.sangupta/projects/jerry
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.sangupta.jerry.web.filters;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sangupta.jerry.web.HttpServletResponseWrapperImpl;
+
+/**
+ * A Servlet {@link Filter} that reorganizes and pushes all JavaScript
+ * files and tags to the end of the HTML. It uses JSoup as the document
+ * parser to achieve the same.
+ * 
+ * @author sangupta
+ *
+ */
+public class JavascriptReorderingFilter implements Filter {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JavascriptReorderingFilter.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// do nothing
+	}
+
+	@Override
+	public void destroy() {
+		// do nothing
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		// try and see if this request is for an HTML page
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        String uri = request.getRequestURI();
+        final boolean htmlPage = isHtmlPage(uri);
+
+        // if this is an HTML page, get the entire contents of the page
+        // in a byte-stream
+        if (!htmlPage) {
+            LOGGER.debug("Not an HTML page for javascript reordering: {}", uri);
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        // run through a wrapper response object
+        HttpServletResponseWrapperImpl wrapper = new HttpServletResponseWrapperImpl((HttpServletResponse) servletResponse);
+        filterChain.doFilter(servletRequest, wrapper);
+
+        // check if this is an HTML output
+        if (!"text/html".equals(wrapper.getContentType())) {
+            LOGGER.debug("Response content not HTML for javascript reordering: {}", uri);
+
+            // just write the plain response
+            // this is not HTML response
+            wrapper.copyToResponse(servletResponse);
+            return;
+        }
+
+        final long startTime = System.currentTimeMillis();
+        LOGGER.debug("Javascript reordering candidate found: {}", uri);
+
+        writeReorderedHtml(wrapper, servletResponse);
+        final long endTime = System.currentTimeMillis();
+
+        LOGGER.debug("Reordering javascript for url: {} took: {}ms", uri, (endTime - startTime));
+	}
+
+	private void writeReorderedHtml(HttpServletResponseWrapperImpl wrapper, ServletResponse servletResponse) throws IOException {
+        // this is an HTML response
+        // we must re-order javascript
+        // flush the entire chunk that may need to be written
+        wrapper.flushBuffer();
+
+        final byte[] bytes = wrapper.getBytes();
+        final String encoding = wrapper.getCharacterEncoding();
+
+        String htmlCode = null;
+        if (encoding != null) {
+            try {
+                htmlCode = new String(bytes, encoding);
+            } catch (UnsupportedEncodingException e) {
+                LOGGER.error("Unable to encode byte response to string for encoding: " + encoding, e);
+            }
+        } else {
+            htmlCode = new String(bytes);
+        }
+
+        if (htmlCode == null) {
+            LOGGER.error("Unable to construct HTML code from byte-stream");
+            wrapper.copyToResponse(servletResponse);
+            return;
+        }
+
+        // parse this html code
+        final Document document = Jsoup.parse(htmlCode);
+
+        boolean jsModified = updateJavascripts(document);
+        boolean cssModified = updateCSS(document);
+
+        // has something been modified?
+        if (!jsModified && !cssModified) {
+            wrapper.copyToResponse(servletResponse);
+            return;
+        }
+
+        // get the byte-array for this html
+        byte[] alteredResponse = document.outerHtml().getBytes(encoding);
+
+        // send back this HTML response
+        ((HttpServletResponse) servletResponse).addHeader("X-Unity-Reordered", "true");
+        wrapper.copyToResponse(servletResponse, alteredResponse);
+    }
+
+    private boolean updateCSS(Document document) {
+        // only switch over files inside the BODY tag
+        // the ones in the HEAD are already at the right place
+        Elements elements = document.body().getElementsByTag("link");
+        if (elements == null || elements.size() == 0) {
+            // no javascript tag found
+            LOGGER.debug("No CSS-link tags found");
+            return false;
+        }
+
+        final List<String> sourceFiles = new ArrayList<String>();
+
+        for (Element element : elements) {
+            String type = element.attr("type");
+            if (!"text/css".equals(type)) {
+                continue;
+            }
+
+            // add to our list of movement
+            sourceFiles.add(element.attr("href"));
+
+            // remove from source
+            element.remove();
+        }
+
+        LOGGER.debug("Found {} CSS files to be reordered", sourceFiles.size());
+
+        // reconstruct the HTML codebase again
+        for (String sourceDocument : sourceFiles) {
+            Element link = document.head().appendElement("link");
+            link.attr("type", "text/css");
+            link.attr("rel", "stylesheet");
+            link.attr("href", sourceDocument);
+        }
+
+        return true;
+    }
+
+    private boolean updateJavascripts(Document document) {
+        // reorder javascript
+        Elements elements = document.getElementsByTag("script");
+        if (elements == null || elements.size() == 0) {
+            // no javascript tag found
+            LOGGER.debug("No javascript tag found");
+            return false;
+        }
+
+        final List<String> sourceFiles = new ArrayList<String>();
+        final List<Boolean> sourceType = new ArrayList<Boolean>();
+
+        for (Element element : elements) {
+            // get the source attribute
+            String source = element.attr("src");
+            if (source != null && source.length() > 0) {
+                sourceFiles.add(source);
+                sourceType.add(true);
+            } else {
+                // not a source file
+                // has code inside
+                for (DataNode dn : element.dataNodes()) {
+                    sourceFiles.add(dn.getWholeData());
+                }
+                sourceType.add(false);
+            }
+
+            // remove element from document
+            element.remove();
+        }
+
+        // output debug messages
+        LOGGER.debug("Found {} source files/code snippets", sourceFiles.size());
+
+        // reconstruct the HTML codebase again
+        for (int index = 0; index < sourceFiles.size(); index++) {
+            String sourceDocument = sourceFiles.get(index);
+            Boolean isSourcePath = sourceType.get(index);
+
+            Element element = document.body().appendElement("script");
+            if (isSourcePath) {
+                element.attr("type", "text/javascript");
+                element.attr("src", sourceDocument);
+            } else {
+                DataNode dn = new DataNode(sourceDocument, element.baseUri());
+                element.appendChild(dn);
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isHtmlPage(String uri) {
+        // find extension
+        int index = uri.lastIndexOf('.');
+        if (index == -1) {
+            return false; // no extension found
+        }
+
+        String currentExtension = uri.substring(index);
+        if (".html".equals(currentExtension)) {
+            return true;
+        }
+
+        // nothing found
+        return false;
+    }
+}
