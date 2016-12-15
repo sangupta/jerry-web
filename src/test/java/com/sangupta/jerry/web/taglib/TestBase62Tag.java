c7ebcd0beb16a86e3c06aa155752e0bf0308f4ae
Sandeep Gupta <sandy.pec@gmail.com>
2016-12-15 11:42:57 +0530

    Started working on unit-test cases

---
+++ src/test/java/com/sangupta/jerry/web/taglib/TestBase62Tag.java
@@ -0,0 +1,40 @@
+package com.sangupta.jerry.web.taglib;
+
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sangupta.am.servlet2.helper.AmTagLibTestHelper;
+import com.sangupta.jerry.encoder.Base62Encoder;
+
+public class TestBase62Tag {
+	
+	@Test
+	public void testEncode() {
+		Random random = new Random();
+		for(int run = 0; run < 1000; run++) {
+			long value = random.nextLong();
+
+			Base62Tag tag = AmTagLibTestHelper.getTagForUnitTesting(Base62Tag.class);
+			tag.setEncode(value);
+			AmTagLibTestHelper.doTag(tag);
+			Assert.assertEquals(Base62Encoder.encode(value), AmTagLibTestHelper.getJspWriter(tag).toString());
+		}
+	}
+	
+	@Test
+	public void testDecode() {
+//		Random random = new Random();
+//		for(int run = 0; run < 1000; run++) {
+//			long value = random.nextLong();
+//			
+//			Base62Tag tag = AmTagLibTestHelper.getTagForUnitTesting(Base62Tag.class);
+//			tag.setDecode(Base62Encoder.encode(value));
+//			AmTagLibTestHelper.doTag(tag);
+//			
+//			Assert.assertEquals(String.valueOf(value), AmTagLibTestHelper.getJspWriter(tag).toString());
+//		}
+	}
+
+}
---
+++ src/test/java/com/sangupta/jerry/web/taglib/TestBase64Tag.java
@@ -0,0 +1,38 @@
+package com.sangupta.jerry.web.taglib;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sangupta.am.servlet2.helper.AmTagLibTestHelper;
+import com.sangupta.jerry.encoder.Base64Encoder;
+import com.sangupta.jerry.util.StringUtils;
+
+public class TestBase64Tag {
+	
+	@Test
+	public void testEncode() {
+		for(int run = 0; run < 1000; run++) {
+			String random = StringUtils.getRandomString(2048);
+
+			Base64Tag tag = AmTagLibTestHelper.getTagForUnitTesting(Base64Tag.class);
+			tag.setEncode(random);
+			AmTagLibTestHelper.doTag(tag);
+			
+			Assert.assertEquals(Base64Encoder.encodeToString(random.getBytes(), false), AmTagLibTestHelper.getJspWriter(tag).toString());
+		}
+	}
+
+	@Test
+	public void testDecode() {
+		for(int run = 0; run < 1000; run++) {
+			String random = StringUtils.getRandomString(2048);
+			String encoded = Base64Encoder.encodeToString(random.getBytes(), false);
+			
+			Base64Tag tag = AmTagLibTestHelper.getTagForUnitTesting(Base64Tag.class);
+			tag.setDecode(encoded);
+			AmTagLibTestHelper.doTag(tag);
+			
+			Assert.assertEquals(random, AmTagLibTestHelper.getJspWriter(tag).toString());
+		}
+	}
+}
